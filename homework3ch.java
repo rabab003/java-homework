172c32191f0d8cfcf5ee426c66c23d164cbeca99
rabab003 <bintaliraq4@gmail.com>
2026-02-01 04:43:55 +0300

    third homwork has been upadated

--- homework3ch.java
+++ homework3ch.java
@@ -68,10 +68,12 @@ public class homework3ch {
         int num3 = input.nextInt();
 
 
-        if(num1 > num2 && num1 > num3){
-            System.out.println("number 1 is the largest");
-        }else if(num2 > num3){
-            System.out.println("number 2 is larger than number 3");
+        if(num1 >= num2 && num1 >= num3) {
+            System.out.println("Number 1 is the largest");
+        } else if(num2 >= num1 && num2 >= num3) {
+            System.out.println("Number 2 is the largest");
+        } else {
+            System.out.println("Number 3 is the largest");
         }
     }
 
